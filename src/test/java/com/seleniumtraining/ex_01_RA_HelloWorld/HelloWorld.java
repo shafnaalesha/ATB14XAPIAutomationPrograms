2a29f85786f4ee2679798f98e4728f4de4a507bb
Shafna Shamsudeen <shafnaalesha@gmail.com>
2025-12-04 19:13:19 +0530

    postman

---
+++ src/test/java/com/seleniumtraining/ex_01_RA_HelloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.seleniumtraining.ex_01_RA_HelloWorld;
+import io.restassured.RestAssured;
+public class HelloWorld {
+    public static void main(String[] args) {
+        RestAssured.given().baseUri("https://google.com").log().all()
+                .when().get()
+                .then().statusCode(200).log().all();
+    }
+}
